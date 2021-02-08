4fe2f568dbcd4ac1d8b31d4acf7b349936313ffe
João Pedro <jpcar200@gmail.com>
2021-02-08 13:21:15 -0800

    Adiconado inserção de nova musica

--- scripts.js
+++ scripts.js
@@ -19,23 +19,18 @@ musics = [
         image_description: "https://i.pinimg.com/originals/0a/fa/62/0afa628528a62ebc8b594ff0c40bda68.jpg",
         song_title: "Bad Liar",
         lyric: "So look me in the eyes \n Tell me what you see \n Perfect paradise \n Tearing at the seams \n I wish \n I could escape \n I  don't wanna fake it \n Wish I could erase it \n Make your heart believe \n But I'm a \n bad liar, bad liar \n Now you know \n Now you know \n I'm a bad liar, bad liar \n Now you know, you're \n to go (go)"
-    },
-    {
-        id: 2,
-        image_description: "https://i.pinimg.com/originals/0a/fa/62/0afa628528a62ebc8b594ff0c40bda68.jpg",
-        song_title: "Next to me",
-        lyric: "So look me in the eyes \n Tell me what you see \n Perfect paradise \n Tearing at the seams \n I wish \n I could escape\n I don't wanna fake it \n Wish I could erase it \n Make your heart believe \n But I'm a  \n bad liar, bad liar \n Now you know \n Now you know \n I'm a bad liar, bad liar \n Now you know, you're \n  to go (go)"
-    },
-    {
-        id: 3,
-        image_description: "https://i.pinimg.com/originals/0a/fa/62/0afa628528a62ebc8b594ff0c40bda68.jpg",
-        song_title: "Demons",
-        lyric: "So look me in the eyes \n Tell me what you see \n Perfect paradise \n Tearing at the seams \n I wish \n I could escape\n I don't wanna fake it \n Wish I could erase it \n Make your heart believe \n But I'm a  \n bad liar, bad liar \n Now you know \n Now you know \n I'm a bad liar, bad liar \n Now you know, you're \n  to go (go)"
     }
 ],
 
 Music = {
 
+    allMusics: musics,
+
+    add(music){
+        Music.allMusics.push(music)
+        console.log(music)
+        App.reload()
+    }
 },
 
 DOM = {
@@ -57,6 +52,60 @@ DOM = {
                 <p class="lyrics" alt="Parte da letra da musica"> ${music.lyric} </p>
             `
         return html
-    }
+    },
 
+},
+
+Form = {
+    image_description: document.getElementById('image_description'),
+    song_title: document.getElementById('song_title'),
+    lyric: document.getElementById('lyrics'),
+
+    getValues(){
+        return{
+            image_description: Form.image_description.value,
+            song_title: Form.song_title.value,
+            lyric: Form.lyric.value
+        }
+    },
+
+    validateFields(){
+        const {image_description, song_title, lyric} = this.getValues()
+
+        if (image_description.trim() === "" ||
+            song_title.trim() === "" ||
+            lyric.trim() === "") {
+            throw new Error("Por favor, preencha todos os campos")
+        }
+    },
+
+    clearFields() {
+        Form.image_description.value = ""
+        Form.song_title.value = ""
+        Form.lyric.value = ""
+    },
+
+    submit(event){
+        event.preventDefault()
+        try {
+            Form.validateFields()
+            const music = Form.getValues()
+            Music.add(music)
+            Form.clearFields()
+            Modal.close()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+},
+
+App = {
+    init(){
+        Music.allMusics.forEach(DOM.addMusic)
+    },
+    reload(){
+        App.init()
+    }
 }
+
+App.init()
